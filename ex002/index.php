60100d7d63626d015f5a1b53ab5a58064fd03d58
Vicrrs <enricofermi2019@gmail.com>
2023-03-12 04:46:10 -0400

    var

---
+++ ex002/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo php</title>
+</head>
+
+<body>
+    <h1>EXEMPLO PHP</h1>
+    <?php 
+        $nome = "Victor Roza";
+        echo("Meu nome é $nome");
+    ?>
+</body>
+
+</html>
